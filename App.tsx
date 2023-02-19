76bbba809574f584f58c865f3ac5ec7b512efd53
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-02-19 13:12:30 +0100

    Hola mundo

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+  
+
+  return (
+    <SafeAreaView>
+      <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
